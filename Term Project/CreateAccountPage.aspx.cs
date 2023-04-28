a68832114e579f4cbbcdb1837f5b8612eb2067cb
JamesFellers1 <124087000+JamesFellers1@users.noreply.github.com>
2023-04-28 11:40:20 -0400

    Update

--- Term Project/CreateAccountPage.aspx.cs
+++ Term Project/CreateAccountPage.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using Utilities;
 
 namespace Term_Project
 {
@@ -11,11 +14,49 @@ namespace Term_Project
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack){
+            
+            }
         }
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
+            String encryptedPW = Encryptor.Encrypt(txtPassword.Text);
 
+            SqlCommand myCommand = new SqlCommand();
+            myCommand.CommandType = CommandType.StoredProcedure;
+            myCommand.CommandText = "TP_CreateAccount"; //name of stored procedure
+
+            myCommand.Parameters.AddWithValue("@username", txtUsername.Text); //to add an input parameter
+            myCommand.Parameters.AddWithValue("@password", encryptedPW);
+            myCommand.Parameters.AddWithValue("@email", txtEmail.Text);
+            myCommand.Parameters.AddWithValue("@name", txtName.Text);
+            myCommand.Parameters.AddWithValue("@address", txtAddress.Text);
+            myCommand.Parameters.AddWithValue("@phone", txtPhone.Text);
+            myCommand.Parameters.AddWithValue("@type", radType.SelectedValue);
+            myCommand.Parameters.AddWithValue("@sq1", ddlSQ1.SelectedValue);
+            myCommand.Parameters.AddWithValue("@sq2", ddlSQ2.SelectedValue);
+            myCommand.Parameters.AddWithValue("@sq3", ddlSQ3.SelectedValue);
+            myCommand.Parameters.AddWithValue("@sq1Ans", txtSQ1.Text);
+            myCommand.Parameters.AddWithValue("@sq2Ans", txtSQ2.Text);
+            myCommand.Parameters.AddWithValue("@sq3Ans", txtSQ3.Text);
+
+            DBConnect myDB = new DBConnect();
+            DataSet myDataSet;
+            myDataSet = myDB.GetDataSet(myCommand);
+
+            //Create session objects
+            Session["Email"] = txtEmail.Text;
+            Session["Name"] = txtName.Text;
+
+            //Store a cookie 
+            HttpCookie userInfo = new HttpCookie("userInfo");
+            userInfo["UserName"] = txtUsername.Text;
+            userInfo["Password"] = encryptedPW;
+            userInfo.Expires = DateTime.Now.AddHours(1);
+            Response.Cookies.Add(userInfo);
+
+
+            Response.Redirect("VerificationPage.aspx");
         }
     }
 }
--- Term Project/LoginPage.aspx.cs
+++ Term Project/LoginPage.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using Utilities;
 
 namespace Term_Project
 {
@@ -14,14 +17,41 @@ namespace Term_Project
 
         }
 
-        protected void btnSubmit_Click(object sender, EventArgs e)
-        {
-
-        }
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
+            //encrypt pw
+            String encryptedPW = Encryptor.Encrypt(txtPassword.Text);
+            String decryptedPW = Encryptor.Decrypt(encryptedPW);
 
+            //Add Cookie
+            HttpCookie userInfo = new HttpCookie("userInfo");
+            userInfo["UserName"] = txtUsername.Text;
+            userInfo["Password"] = encryptedPW;
+            userInfo.Expires = DateTime.Now.AddHours(1);
+            Response.Cookies.Add(userInfo);
+
+            SqlCommand myCommand = new SqlCommand();
+            myCommand.CommandType = CommandType.StoredProcedure;
+            myCommand.CommandText = "TP_Login";
+            
+            myCommand.Parameters.AddWithValue("@username", txtUsername.Text);
+            myCommand.Parameters.AddWithValue("@password", txtPassword.Text);
+            
+
+            DBConnect checkLogin = new DBConnect();
+            DataSet loginData;
+            loginData = checkLogin.GetDataSet(myCommand);
+            if (loginData.Tables[0].Rows.Count == 0 || loginData == null)
+            {
+                lblError.Text = "Invalid Login, Please try again.";
+            }
+            else
+            {
+                string str = loginData.Tables[0].Rows[0]["UserId"].ToString();
+                Session["UserID"] = str;
+                Response.Redirect("SearchHomesPage.aspx");
+            }
         }
     }
 }
